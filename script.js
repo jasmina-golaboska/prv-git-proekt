91e171fa6e37e653bcaa33eddba08d3334de6252
Jasmina Golaboska <sutinoska@yahoo.com>
2017-12-28 21:11:31 +0100

    prv initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let ulTag = document.getElementById("lista");
+
+let person = {
+    name: "Jasmina",
+    lastName: "Golaboska",
+    age: 38,
+    phones: ["0713726172", "0773827261", "076546227"]
+};
+
+for (let i =0; i < person.phones.length; i=i+1) {
+let newLi = document.createElement("li");
+newLi.innerHTML = person.phones[i];
+    ulTag.appendChild (newLi);
+};
